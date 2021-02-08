3934d9de807774fe1c91904221aa41a309a72eb7
Mauricio Rojas <mrojasb2000@gmail.com>
2021-02-07 22:49:15 -0300

    Added get book by id and filters on query params

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const fastify = require("fastify")
 const server = fastify()
 
-server.get('/', async function(request, reply) {
-      reply.send({
-          message: "Fastify works"
-      })
+const books = [
+    "El arte de la guerra",
+    "El laberinto de la soledad",
+    "Rebelión en la granja"
+]
+
+server.get('/books', async function(request, reply) {
+    const filter = request.query.filter
+
+    if (filter) {
+        reply.send({
+            data: books.filter(book => book.includes(filter)),
+        })
+    }
+
+    reply.send({
+        message: "Fastify works",
+        data: books,
+    })
 })
+
+server.get("/book/:id", (request, reply) => {
+    const id = request.params.id
+    reply.send({
+        data: id,
+    })
+})
+
 
 server.listen(3000, function(error) {
     if (error) {
